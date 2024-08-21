96575611ca8c369141ccf4df682a2d7bf1c71446
Nada Shetty <nada.shetty@yahoo.com>
2024-08-21 23:21:56 +0100

    update header styling and spacing for ResourceCard

--- src/components/ResourceContent/ResourceCard.tsx
+++ src/components/ResourceContent/ResourceCard.tsx
@@ -24,12 +24,12 @@ const ResourceCard: React.FC<ResourceCardProps> = ({
 }) => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
-          <h4 className="text-title-md font-bold text-primary">{title}</h4>
-          <p className="text-md py-4 font-medium text-black dark:text-white">
+          <h4 className="pb-2 text-title-md font-bold text-primary">{title}</h4>
+          <p className="text-md font-medium text-black dark:text-white">
             {description}
           </p>
-          <p className="py-4 text-title-md font-bold text-primary">Links</p>
-          <p className="py-4">
+          <p className="pt-4 pb-2 text-title-md font-bold dark:text-white">Links</p>
+          <p>
             {links.map((link, index) => (
               <div key={index}>
                 <a href={link} target="_blank" className="text-blue-600">
@@ -39,30 +39,30 @@ const ResourceCard: React.FC<ResourceCardProps> = ({
               </div>
             ))}
           </p>
-          <p className="py-4 text-title-md font-bold text-primary">Image</p>
+          <p className="pt-4 pb-2 text-title-md font-bold dark:text-white">Image</p>
           <img src={imgSrc} alt="placeholder" />
-          <p className="py-4 text-title-md font-bold text-primary">Videos</p>
+          <p className="pt-4 pb-2 text-title-md font-bold dark:text-white">Videos</p>
           <div>
             {videos.map((video, index) => (
               <div key={index}>
-                <p className="py-4 text-title-sm font-medium text-black dark:text-white">
+                <p className="pb-2 text-title-sm font-bold text-primary">
                   {video.title}
                 </p>
-                <p className="text-md py-4 font-medium text-black dark:text-white">
+                <p className="text-md font-medium text-black dark:text-white">
                   {video.description}
                 </p>
                 <p>
                   <a href={video.link} target="_blank" className="text-blue-600">{video.link}</a>
                 </p>
-                <p className="text-md py-4 font-medium text-black dark:text-white">
+                <p className="pb-4 text-md font-medium text-black dark:text-white">
                   {video["further-details"]}
                 </p>
               </div>
             ))}
-            <p className="py-4 text-title-md font-bold text-primary">
+            <p className="pb-2 text-title-md font-bold dark:text-white">
               More resource details
             </p>
-            <p className="text-md py-4 font-medium text-black dark:text-white">
+            <p className="text-md font-medium text-black dark:text-white">
               {resourceText}
             </p>
           </div>
